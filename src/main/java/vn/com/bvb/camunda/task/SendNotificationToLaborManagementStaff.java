c406a4c392d161f83e5dff20d2202b24cdc5c3f5
hungmt <hungmt5490@gmail.com>
2024-01-13 21:14:13 +0700

    delete system.out

--- src/main/java/vn/com/bvb/camunda/task/SendNotificationToLaborManagementStaff.java
+++ src/main/java/vn/com/bvb/camunda/task/SendNotificationToLaborManagementStaff.java
@@ -2,14 +2,18 @@ package vn.com.bvb.camunda.task;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SendNotificationToLaborManagementStaff implements JavaDelegate {
+	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        System.out.println("Send Notification To Labor Management Staff");
+    	logger.info("Send Notification To Labor Management Staff");
     }
 }
 
