392ecc8de25084a7ded72bca604b21b8803cd114
hungmt <hungmt5490@gmail.com>
2024-01-13 20:47:54 +0700

    fix

--- src/main/java/vn/com/bvb/camunda/listener/complete/DoneRecruitmentListener.java
+++ src/main/java/vn/com/bvb/camunda/listener/complete/DoneRecruitmentListener.java
@@ -2,8 +2,8 @@ package vn.com.bvb.camunda.listener.complete;
 
 import java.util.List;
 
-import org.camunda.bpm.engine.delegate.DelegateTask;
-import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,7 +18,7 @@ import vn.com.bvb.repository.RecruitmentUserTaskRepository;
 
 @Component
 @AllArgsConstructor
-public class DoneRecruitmentListener  implements TaskListener {
+public class DoneRecruitmentListener  implements ExecutionListener {
 	
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -28,13 +28,12 @@ public class DoneRecruitmentListener  implements TaskListener {
 	
 	@Override
 	@Transactional
-	public void notify(DelegateTask delegateTask) {
-		String processInstanceId = delegateTask.getProcessInstanceId();
-		String businessKey = delegateTask.getExecution().getProcessInstance().getBusinessKey();
-		String assignee = delegateTask.getAssignee();
+	public void notify(DelegateExecution execution) {
+		String processInstanceId = execution.getProcessInstanceId();
+		String businessKey = execution.getProcessInstance().getBusinessKey();
 
-		logger.info("processInstanceId = {}, businessKey = {}, assignee = {}", 
-				processInstanceId, businessKey, assignee);
+		logger.info("processInstanceId = {}, businessKey = {}", 
+				processInstanceId, businessKey);
 		
 		Employee employee = employeeRepository.findByCode(businessKey)
 				.orElseThrow(() -> new NullPointerException("Not Found Employee with code=" + businessKey));
